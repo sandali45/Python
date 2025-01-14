5ece614bb3b9ad28f0471f1a26e664ff949a5492
sandali45 <139307298+sandali45@users.noreply.github.com>
2025-01-14 20:25:04 +0530

    Python
    Python Day 01

---
+++ Python Basics.py
@@ -0,0 +1,13 @@
+#string #Integer #float # boolean
+name="kai vanos"
+age=32
+gpa=4.42
+print("Hello kai vanos")
+print(f"Age is {age} ! welcome!")
+print(f"GPA is {gpa}!")
+wealthy =True
+if wealthy:
+    print("I am wealthy")
+else:
+    print("I am not wealthy")
+
