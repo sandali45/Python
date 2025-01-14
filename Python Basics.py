8bd6d0b739487fc90dc7a159d069bc084b8d819a
sandali45 <139307298+sandali45@users.noreply.github.com>
2025-01-14 20:36:15 +0530

    python
    Python Day 01 { Basic]

--- Python Basics.py
+++ Python Basics.py
@@ -10,4 +10,29 @@ if wealthy:
     print("I am wealthy")
 else:
     print("I am not wealthy")
+print("--------------------------------------------------------")
+x,y,z="kai vanos" ,32 ,"wealthy"
+print(x)
+print(y)
+print(z)
+print("--------------------------------------------------------")
+#list
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
+print("--------------------------------------------------------")
+a=5
+b=10
+print(f"The value is {a+b}")
+print("--------------------------------------------------------")
+x = "awesome"
 
+def myfunc():
+  print("Python is " + x)
+
+myfunc()
+print("--------------------------------------------------------")
+x = 5
+print(type(x))
