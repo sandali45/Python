5a41c5210aa5d872ba81331d35fe36423d76b3b0
sandali45 <139307298+sandali45@users.noreply.github.com>
2025-01-14 20:54:15 +0530

    python
    Python Day 01 [Basic]

--- Python Basics.py
+++ Python Basics.py
@@ -36,3 +36,33 @@ myfunc()
 print("--------------------------------------------------------")
 x = 5
 print(type(x))
+
+#Type casting
+gpa=4.0 #float type
+print(gpa)#float
+print(int(gpa)) #print as an Int
+
+name="kai vanos"
+name = bool(name)
+print(name)
+name2="kai vanos"
+print(bool(name2))
+name3=""
+print(bool(name3))
+print("--------------------------------------------------------")
+a = "Hello, World!"
+print(a[1])
+print(a[2])
+#Get the characters from position 2 to position 5 (not included):
+b = "Hello, World!"
+print(b[2:5])
+#Get the characters from the start to position 5 (not included):
+b = "Hello, World!"
+print(b[:5])
+print(b[:-2])
+#in
+txt = "The best things in life are free!"
+print("free" in txt)
+#no
+txt = "The best things in life are free!"
+print("expensive" not in txt)
