64de7cb820b7b906f147dec2cf5155c3c2207873
sandali45 <139307298+sandali45@users.noreply.github.com>
2025-01-15 23:54:26 +0530

    Python
    Python Day 2

--- Python Basic2.py
+++ Python Basic2.py
@@ -1,7 +1,21 @@
 a = "kai vanos!"
 print(f"lower case: {a.lower()}")
-print(f"upper case: {a.upper()}")
+print(f"upper case: {a.upper()}")#place holder 
+# f-string, simply put an f in front of the string literal, and add curly brackets {} as placeholders for variables and other operations
 print(a.strip())
 print(a.replace("a", "s"))#all same letters replace by this all äs in this will be replaced
 print(a.split(","))
 print("----------------------------------------------------------------")
+a = "kai"
+b = "vanos"
+c = a + b
+print(c)
+a = "kai"
+b = "vanos"
+c = a + " " + b
+print(c)
+print("----------------------------------------------------------------")
+age = 32
+txt = "My name is " + c + ", I am " + str(age)
+print(txt)
+print(f"the total is:{20+20}")
